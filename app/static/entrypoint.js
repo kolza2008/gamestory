bb96dc2d36d36a2148f0e0003e45cda743d6fdae
kolza2008 <kolza2008@bk.ru>
2022-01-10 18:27:46 +0300

    Start work for push notifications

--- app/static/entrypoint.js
+++ app/static/entrypoint.js
@@ -1,3 +1,28 @@
+
+var key = urlBase64ToUint8Array(document.getElementById('notify_key').content)//getKey()
+
+function urlBase64ToUint8Array(base64String) {
+    var padding = '='.repeat((4 - base64String.length % 4) % 4);
+    var base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    var rawData = window.atob(base64);
+    var outputArray = new Uint8Array(rawData.length);
+
+    for (var i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+};
+
+var sendSubscriptionToBackend = function(subscription){
+    console.log(JSON.stringify(subscription));
+    fetch('/subscribe', {'method':'post', 'body': JSON.stringify(subscription)})
+    $.post('/subscribe',
+           {'object':JSON.stringify(subscription)});
+};
+
 let subscribe = function(){
     // Проверка поддержки браузером уведомлений
     if (!("Notification" in window)) {
@@ -23,12 +48,19 @@ let subscribe = function(){
     if (!('PushManager' in window)) {
         alert('Ваш браузер не поддерживает пуш-уведомления')
         return;
-      }
+    };
     if(!('serviceWorker' in navigator)){
         alert('Ваш браузер не поддерживает фоновые задачи');
         return;
     };
-    navigator.serviceWorker.register('/sw.js').then(()=>console.log('service worker loaded'));
-    
+    navigator.serviceWorker.register('/sw').then(
+        function(registration){
+            var subscribeOptions = {
+                userVisibleOnly: true,
+                applicationServerKey: key,
+            };
+            registration.pushManager.subscribe(subscribeOptions).then(subscription => sendSubscriptionToBackend(subscription)); 
+        }
+    );
 };
  
