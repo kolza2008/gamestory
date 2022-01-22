ca0d1d736691d7acbaae2fb563ec3be6df1bd10a
kolza2008 <kolza2008@bk.ru>
2022-01-22 21:01:01 +0300

    Fix mysql connection query

--- config.py
+++ config.py
@@ -20,4 +20,6 @@ class ConfigOnServer(ConfigOnTest):
     APP_URL = 'kolza2008.pythonanywhere.com'
     MAX_CONTENT_LENGHT = 100 * 1024 * 1024
     PATH_TO_APP = "/home/kolza2008/mysite/"
+    SQLALCHEMY_POOL_TIMEOUT = 20
+    SQLALCHEMY_POOL_RECYCLE = 299
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://kolza2008:afganistan1457@kolza2008.mysql.pythonanywhere-services.com/kolza2008$main'
