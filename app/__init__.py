c0e114ef980fa05619f630aef4d4689260ebd069
kolza2008 <kolza2008@bk.ru>
2022-01-22 18:13:57 +0300

    cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 import os
 from flask import *
+from flask_cors import *
+from flask_mail import Mail 
 from flask_sqlalchemy import *
+from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 
@@ -8,8 +11,11 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 app.config.from_object(os.environ.get('CONFIG') or 'config.ConfigOnTest')
 
+mail = Mail(app)
+cors = CORS(app)
 bs = Bootstrap(app)
 db = SQLAlchemy(app)
+migr = Migrate(app, db)
 lm = LoginManager(app)
 lm.login_view = 'login'
 
